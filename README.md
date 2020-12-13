9fbe1f760cd9aa33d00d9bd2920f5bb535e68afd
redneran <t-narender@talasecurity.io>
2020-12-13 15:53:30 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# snake-ladder #
+
+## Getting Started
+
+### Dependencies
+
+1. Java (1.8 or above) & Maven (3.5 or above)
---
+++ src/main/java/com/deserve/snakeladder/App.java
@@ -0,0 +1,13 @@
+package com.deserve.snakeladder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
